c1d1e867226d1a211e7c5e54bdd71a804c8d6147
Robert Geithe <robert.geithe@endava.com>
2021-07-24 01:48:32 +0200

    * shaman non combat heal tweaks

--- playerbot/strategy/shaman/ShamanNonCombatStrategy.cpp
+++ playerbot/strategy/shaman/ShamanNonCombatStrategy.cpp
@@ -46,6 +46,14 @@ void ShamanNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
         NextAction::array(0, new NextAction("chain heal", 30.0f), NULL)));
 
     triggers.push_back(new TriggerNode(
+        "low health",
+        NextAction::array(0, new NextAction("healing wave", ACTION_CRITICAL_HEAL + 2), NULL)));
+
+    triggers.push_back(new TriggerNode(
+        "party member low health",
+        NextAction::array(0, new NextAction("healing wave on party", ACTION_CRITICAL_HEAL + 1), NULL)));
+
+    triggers.push_back(new TriggerNode(
         "critical aoe heal",
         NextAction::array(0, new NextAction("chain heal", ACTION_CRITICAL_HEAL), NULL)));
 
